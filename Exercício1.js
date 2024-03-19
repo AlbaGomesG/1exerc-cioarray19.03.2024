f889f5e607e29aa613840be81d8c3cdf83df7fcf
AlbaGomesG <giovannaalba.tds1.senai@gmail.com>
2024-03-19 10:28:29 -0300

    First commit

---
+++ Exercício1.js
@@ -0,0 +1,7 @@
+let frase = "Prazer, eu sou a Giovanna!";
+let letras = frase.split("");
+let vogais = letras.filter((letras) => ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"].includes(letras.toLowerCase()));
+
+console.log("texto:", frase);
+console.log("número total de vogais:", vogais.length);
+console.log("vogais encontradas:", vogais);
